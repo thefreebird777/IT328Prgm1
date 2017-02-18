a17b3339346666148777ca3094ef5614953f8bd7
coe_ileyden <ileyden@ilstu.edu>
2017-02-17 20:41:16 -0600

    Merge branch 'master' of C:\Users\coe_ileyden\IdeaProjects\IT328Prgm1 with conflicts.

--- src/solveClique.java
+++ src/solveClique.java
@@ -1,5 +1,3 @@
-package src;
-import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
--- src/solveISet.java
+++ src/solveISet.java
@@ -1,61 +1,49 @@
-package src;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class solveISet {
-	
-	public static int[][] graph;
-	public static int vertices, numGraphs, edges, row;
-	public static long timeMS;
+    public static int[][] graph;
+    public static int edges, vertices, numGraphs, row;
+    public static long timeMS;
 
-	/**
-	 * Default Constructor
-	 */
-	public solveISet() {
-		graph = new int[60][60];
-		vertices = 0;
-		numGraphs = 0;
-		edges = 0;
-		row = 0;
-		timeMS = 0;
-	}
-	public ArrayList<Integer> findMaxISet(ArrayList<Integer> clique, int row, int dim)
-	{
-		long start = System.currentTimeMillis();
-		
-		ArrayList<Integer> tempClique = new ArrayList<Integer>();
-		ArrayList<Integer> maxClique = new ArrayList<Integer>();
-		maxClique = clique;
-		
-		for(int i = row; i < dim; i++)
-		{
-			boolean isClique = true;
-			for(int j = 0; j < clique.size(); j++)
-				if(graph[clique.get(j)][i] != 1) isClique = false;
-			if(tempClique.size() > maxClique.size())
-				maxClique = tempClique;
-		}
-		
-		long end = System.currentTimeMillis();
-		timeMS = end - start;
-		
-		return maxClique;
-	}
-	private static int numEdges(){
-		edges = 0;
-		for(int i = 0; i < vertices; i++)
-		{
-			for(int j = i; j < vertices; j++)
-			{
-				if(graph[i][j] == 1)
-					edges++;
-			}
-		}
-		return edges;
-	}
-	private static void print(ArrayList<Integer> clique)
-	{
-		int size = clique.size();
-		
-		System.out.println("G" + numGraphs + " (" + vertices + ", " + edges + ") " + clique.toString() + "(size=" + clique.size() + ", " + timeMS + " ms)");
-	}
+    public solveISet() {
+        graph = new int[60][60];
+        numGraphs = 0;
+        edges = 0;
+        vertices = 0;
+        row = 0;
+        timeMS = 0;
+    }
+
+    public static void main(String[] args) {
+        solveISet independent = new solveISet();
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("graphs16.txt"));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                vertices = Integer.parseInt(line);
+
+                if (vertices != 0) {
+                    numGraphs++;
+
+                    for (int i = 0; i < vertices; i++) {
+                        for (int j = 0; j < vertices; j++) {
+                            char c = (char) br.read();
+                            int v = Character.getNumericValue(c);
+                            graph[i][j] = v;
+                            numEdges();
+                        }
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file.");
+        } catch (IOException e) {
+            System.out.println("Could not read file.");
+        }
+    }
 }
